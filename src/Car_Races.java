dc161b37f90debe1f5686645b20c613e5e4b461c
Julius <Julius>
2020-05-16 15:34:33 +0300

    Switch another branch

--- src/Car_Races.java
+++ src/Car_Races.java
@@ -5,7 +5,7 @@
 public class Car_Races {
 
     public static void main(String[] args) {
-        System.out.println("Hello here!");
+        System.out.println("Hello GIT / switch to another branch in netBeans");
     }
     
 }
