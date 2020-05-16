8f93d2aebc5d2f0ea271b433a19e61ae52e0bfb5
Julius <Julius>
2020-05-16 16:43:41 +0300

    GET car name, speed and distance and WIN

--- src/Car_Races.java
+++ src/Car_Races.java
@@ -1,3 +1,4 @@
+
 /**
  *
  * @author Julius
@@ -5,35 +6,44 @@
 public class Car_Races {
 
     public static void main(String[] args) {
-        
-        
+
         Car[] cars = {
-        new Car("Audi", 200),
-        new Car("Bmw", 200),
-        new Car("Ferrai", 200),
-        new Car("Toyota", 200),
-        new Car("Skoda", 200)        
+            new Car("Audi", 200),
+            new Car("Bmw", 200),
+            new Car("Ferrai", 200),
+            new Car("Toyota", 200),
+            new Car("Skoda", 200)
         };
-        
+
         do {
             for (int i = 0; i < cars.length; i++) {
                 Car car = cars[i];
-                if(Math.random() <= 0.2 ) {
+                if (Math.random() <= 0.1) {
                     car.deceleration((int) (Math.random() * 4));
                 } else {
                     car.acceleration((int) (Math.random() * 6));
                 }
                 car.start();
             }
-            for(Car car : cars) {
+            for (Car car : cars) {
                 System.out.println(car.getName() + " " + car.getSpeed() + " " + car.getDistance());
             }
-            
-        } while (true);
-        
-        
-        
-        
+            arraySort(cars);
+            System.out.println("--------");
+        } while (cars[0].getDistance() < 200);
+        System.out.println("WIN WIN: " + cars[0].getName());
     }
-    
+
+    public static void arraySort(Car[] cars) {
+        for (int i = 0; i < cars.length - 1; i++) {
+            for (int x = i + 1; i < cars.length; i++) {
+                if(cars[i].getDistance() < cars[x].getDistance()) {
+                    Car temp = cars[i];
+                    cars[i] = cars[x];
+                    cars[x] = temp;
+                }
+            }
+        }
+    }
+
 }
