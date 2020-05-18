bf22864ea7b06bcadb50020ca49c47d43d88aeed
Julius <Julius>
2020-05-19 00:39:44 +0300

    get finish

--- src/SuperCar.java
+++ src/SuperCar.java
@@ -1,27 +1,24 @@
 
 public class SuperCar extends Car {
-    
-    private String spoiler;
+
+    private boolean spoiler;
     private boolean SuperCar;
     private String type;
-    
+
     public SuperCar(String name, int maxSpeed) {
         super(name, maxSpeed);
-        this.spoiler = spoiler;
-        this.SuperCar = true;
+        this.spoiler = true;
         this.type = "Super";
     }
-    
+
     public String getSpoiler() {
-        if(this.getSpeed() > 100) {
+        if (this.getSpeed() > 100) {
             return "+";
         } else {
             return "-";
         }
     }
-    public boolean getSuper() {
-        return SuperCar;
-    }
+
     public String getType() {
         return type;
     }
